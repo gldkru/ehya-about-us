2a6fc2e8705387d7f9e9de1b066509d1cb6ed242
Kirill Gladkikh <onest.kill@yandex.ru>
2022-11-04 19:45:25 +0400

    initial state for form

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,33 @@
 const form = document.getElementById('form')
-const input = document.getElementById('apply')
+const checkbox = document.getElementById('apply')
 const submitBtn = document.getElementById('submit')
+
+const isCheckboxActive = checkbox.hasAttribute('checked')
 
 const submitForm = function (event) {
 	event.preventDefault()
 
 	const domEl = document.querySelector('[name="cto-input"]')
 
+	if (!domEl.value) return
+
 	console.log(event)
 	console.log(domEl.value)
 }
 
-const inputListener = function (event) {
-	if (event.target.checked) {
+const onDisableButton = function (active) {
+	if (active) {
 		submitBtn.removeAttribute('disabled')
 	} else {
 		submitBtn.setAttribute('disabled', 'disabled')
 	}
 }
 
+const inputListener = function (event) {
+	onDisableButton(event.target.checked)
+}
+
 form.addEventListener('submit', submitForm)
-input.addEventListener('change', inputListener)
+checkbox.addEventListener('change', inputListener)
+
+onDisableButton(isCheckboxActive)
